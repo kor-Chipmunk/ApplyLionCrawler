3b79d90855921ac08297c9e390c7edf1f5979296
kor-Chipmunk <rhj4862@cau.ac.kr>
2018-02-25 00:10:22 +0900

    Crawler

---
+++ LikeLionQuestionCrawl/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LikeLionQuestionCrawl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.lvSchool = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnTrue = new System.Windows.Forms.Button();
+            this.btnFalse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.Location = new System.Drawing.Point(13, 579);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(626, 56);
+            this.btnCrawl.TabIndex = 0;
+            this.btnCrawl.Text = "수집하기";
+            this.btnCrawl.UseVisualStyleBackColor = true;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // lvSchool
+            // 
+            this.lvSchool.CheckBoxes = true;
+            this.lvSchool.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvSchool.FullRowSelect = true;
+            this.lvSchool.GridLines = true;
+            this.lvSchool.Location = new System.Drawing.Point(13, 128);
+            this.lvSchool.Name = "lvSchool";
+            this.lvSchool.Size = new System.Drawing.Size(624, 357);
+            this.lvSchool.TabIndex = 2;
+            this.lvSchool.UseCompatibleStateImageBehavior = false;
+            this.lvSchool.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "번호";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "학교 이름";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 200;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(11, 26);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(626, 81);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "로그인하기";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnTrue
+            // 
+            this.btnTrue.Location = new System.Drawing.Point(13, 505);
+            this.btnTrue.Name = "btnTrue";
+            this.btnTrue.Size = new System.Drawing.Size(300, 56);
+            this.btnTrue.TabIndex = 4;
+            this.btnTrue.Text = "모두 체크";
+            this.btnTrue.UseVisualStyleBackColor = true;
+            this.btnTrue.Click += new System.EventHandler(this.btnTrue_Click);
+            // 
+            // btnFalse
+            // 
+            this.btnFalse.Location = new System.Drawing.Point(337, 505);
+            this.btnFalse.Name = "btnFalse";
+            this.btnFalse.Size = new System.Drawing.Size(300, 56);
+            this.btnFalse.TabIndex = 5;
+            this.btnFalse.Text = "모두 체크 해제";
+            this.btnFalse.UseVisualStyleBackColor = true;
+            this.btnFalse.Click += new System.EventHandler(this.btnFalse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 648);
+            this.Controls.Add(this.btnFalse);
+            this.Controls.Add(this.btnTrue);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lvSchool);
+            this.Controls.Add(this.btnCrawl);
+            this.Name = "Form1";
+            this.Text = "자기소개서 항목 크롤링 프로그램";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrawl;
+        private System.Windows.Forms.ListView lvSchool;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnTrue;
+        private System.Windows.Forms.Button btnFalse;
+    }
+}
+
---
+++ LikeLionQuestionCrawl/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace LikeLionQuestionCrawl
+{
+    public partial class Form1 : Form
+    {
+        CookieContainer cookieContainer = new CookieContainer();
+
+        private const string EMAIL = "";
+        private const string PASSWORD = "";
+        private const string SAVEPATH = @"";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            string GetHtml = getHTMLFromWeb("https://apply.likelion.org/users/sign_in");
+            string authToken = ExtractBetweenText(GetHtml, "name=\"authenticity_token\" value=\"", "\"");
+
+            string data = "utf8=%E2%9C%93&authenticity_token=" + HttpUtility.UrlEncode(authToken) + "&" + HttpUtility.UrlEncode("user[email]") + "=" + HttpUtility.UrlEncode(EMAIL) + "&" + HttpUtility.UrlEncode("user[password]") + "=" + HttpUtility.UrlEncode(PASSWORD) + "&commit=%ED%99%95%EC%9D%B8";
+
+            var responseString = postData("https://apply.likelion.org/users/sign_in", data);
+
+            if (responseString.Contains("가입 정보 수정"))
+            {
+                MessageBox.Show("로그인 성공!");
+                GetSchoolList();
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패!");
+            }
+        }
+
+        private async void GetSchoolList()
+        {
+            string GetHtml = getHTMLFromWeb("https://apply.likelion.org/users/edit");
+
+            string[] univs = ExtractBetweenText(GetHtml, "<option value=\"\"></option>", "</select>").Split(new string[] { "<option" }, StringSplitOptions.None);
+
+            lvSchool.Items.Clear();
+
+            for (int i = 1; i < univs.Length; i++)
+            {
+                string num = ExtractBetweenText(univs[i], "value=\"", "\"");
+                string name = ExtractBetweenText(univs[i], ">", "<");
+
+                lvSchool.Items.Add(new ListViewItem(new string[] { num, name }));
+            }
+
+        }
+
+        private string[] CrawlResumeList(int index)
+        {
+            string GetHtml = getHTMLFromWeb("https://apply.likelion.org/users/edit");
+            string authToken = ExtractBetweenText(GetHtml, "name=\"authenticity_token\" value=\"", "\"");
+
+            string number = lvSchool.Items[index].Text;
+
+            string data = "utf8=%E2%9C%93&_method=put&authenticity_token=" + HttpUtility.UrlEncode(authToken) + "&" + HttpUtility.UrlEncode("user[email]") + "=" + HttpUtility.UrlEncode(EMAIL) + "&" + HttpUtility.UrlEncode("user[university_id]") + "=" + number + "&" + HttpUtility.UrlEncode("user[current_password]") + "=qjrm11&commit=" + HttpUtility.UrlEncode("수정");
+
+            var response = postData("https://apply.likelion.org/users", data);
+
+            GetHtml = getHTMLFromWeb("https://apply.likelion.org/application/new");
+
+            string enrollContent = ExtractBetweenText(GetHtml, "<p class=\"enroll__text\">", "</p>");
+
+            string[] items = GetHtml.Split(new string[] { "<h5 class=\"enroll__required\"" }, StringSplitOptions.None);
+
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(enrollContent);
+            enrollContent = htmlDoc.DocumentNode.InnerHtml.Replace("<br>", "\n");
+
+            string[] results = new string[7];
+            
+            results[0] = lvSchool.Items[index].SubItems[1].Text;
+            results[1] = enrollContent;
+
+            for (int i = 0; i < items.Length - 6; i++)
+            {
+                string item = ExtractBetweenText(items[6 + i], ">", "</h5>");
+                htmlDoc.LoadHtml(item);
+
+               results[2 + i] = htmlDoc.DocumentNode.InnerHtml.Replace("<br>", "\n");
+            }
+
+            return results;
+        }
+
+        private void btnTrue_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lvSchool.Items.Count; i++) lvSchool.Items[i].Checked = true;
+        }
+
+        private void btnFalse_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lvSchool.Items.Count; i++) lvSchool.Items[i].Checked = false;
+        }
+
+        private void btnCrawl_Click(object sender, EventArgs e)
+        {
+            int checkedCount = 0;
+
+            // 체크박스 갯수 파악
+
+            for (int i = 0; i< lvSchool.Items.Count; i++)
+            {
+                if (lvSchool.Items[i].Checked == true)
+                    checkedCount++;
+            }           
+            
+            // 크롤링 시작
+
+            Excel.Application excelApp = null;
+            Excel.Workbook wb = null;
+            Excel.Worksheet ws = null;
+
+            excelApp = new Excel.Application();
+            wb = excelApp.Workbooks.Add();
+            ws = wb.Worksheets.Item[1] as Excel.Worksheet;
+
+            // 헤더
+
+            int r = 1;
+            string[] headers = new string[] { "학교 이름", "모집소개", "항목1", "항목2", "항목3", "항목4", "항목5" };
+            string[] results = null;
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                ws.Cells[r, 1 + i] = headers[i];
+            }
+
+            // 크롤링
+            for (int i = 0; i < lvSchool.Items.Count; i++)
+            {
+                if (lvSchool.Items[i].Checked == true)
+                {
+                    results = CrawlResumeList(i);
+
+                    r++;
+
+                    for (int j = 0; j < results.Length; j++)
+                    {
+                        ws.Cells[r, 1 + j] = HttpUtility.HtmlDecode(results[j]);
+                    }
+                }
+            }
+
+            wb.SaveAs(SAVEPATH, Excel.XlFileFormat.xlWorkbookNormal);
+            wb.Close(true);
+            excelApp.Quit();
+           
+            ReleaseExcelObject(ws);
+            ReleaseExcelObject(wb);
+            ReleaseExcelObject(excelApp);
+            
+        }
+
+        private static void ReleaseExcelObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj);
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private string ExtractBetweenText(string Text, string Begin, string End)
+        {
+            return Text.Split(new string[] { Begin }, StringSplitOptions.None)[1].Split(new string[] { End }, StringSplitOptions.None)[0];
+        }
+
+        private string getHTMLFromWeb(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = cookieContainer;
+            request.KeepAlive = true;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Encoding encode = Encoding.GetEncoding("utf-8");
+            Stream strReceive = response.GetResponseStream();
+            StreamReader reqStreamReader = new StreamReader(strReceive, encode);
+            string strResult = reqStreamReader.ReadToEnd();
+
+            return strResult;
+        }
+
+        private string postData(string url, string data)
+        {
+            Byte[] pd = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest  request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.KeepAlive = true;
+            request.ContentLength = pd.Length;
+
+            if (url.Contains("https://apply.likelion.org/users"))
+            {
+                request.Referer = "https://apply.likelion.org/users/edit";
+            }
+
+            request.CookieContainer = cookieContainer;
+
+            Stream sw = request.GetRequestStream();
+            sw.Write(pd, 0, pd.Length);
+            sw.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Encoding encode = Encoding.GetEncoding("utf-8");
+                Stream strReceive = response.GetResponseStream();
+                StreamReader reqStreamReader = new StreamReader(strReceive, encode);
+                string strResult = reqStreamReader.ReadToEnd();
+                request.Abort();
+                strReceive.Close();
+                reqStreamReader.Close();
+
+                return strResult;
+            }
+
+            return "";
+        }
+    }
+}
